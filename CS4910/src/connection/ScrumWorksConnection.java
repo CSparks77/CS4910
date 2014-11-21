e53dae8ef539888969e5f576ac6ebd3934cbe99d
James Manes <JamesManes@iCloud.com>
2014-11-20 18:50:59 -0600

    No comment provided.

--- CS4910/src/connection/ScrumWorksConnection.java
+++ CS4910/src/connection/ScrumWorksConnection.java
@@ -1,29 +1,17 @@
 package connection;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-
-import com.danube.scrumworks.api.client.ScrumWorksEndpoint;
-import com.danube.scrumworks.api.client.ScrumWorksEndpointBindingStub;
-import com.danube.scrumworks.api.client.ScrumWorksServiceLocator;
-import com.danube.scrumworks.api.client.types.BacklogItemWSO;
-import com.danube.scrumworks.api.client.types.ProductWSO;
-import com.danube.scrumworks.api.client.types.ReleaseWSO;
-import com.danube.scrumworks.api.client.types.ServerException;
-import com.danube.scrumworks.api.client.types.SprintWSO;
-
 /**
  * This class is responsible for holding a
  * connection session with the local
  * ScrumWorks Pro installation over the
- * network.
+ * network. 
  * 
- * @author James Manes (JAM38220)
+ * @author James Manes (JAM38220@ucmo.edu)
+ * @author James Sparks (JCS67070@ucmo.edu)
+ * @author Brandt Sanders (BDS01970@ucmo.edu)
  */
 public class ScrumWorksConnection {
-
+	public ScrumWorksConnection() {
+		
+	}
 }
